fc93a558d7fb4dcb5c09cc9a5dedf0e1e7d7c874
carlos <carlosking12@gmail.com>
2021-04-29 09:35:40 -0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,9 @@
+function seta_over(obj){
+    obj.innerHTML = "topo";
+    document.getElementById("p2").style.background = "red";
+}
+
+function seta_out(obj){
+    obj.innerHTML = ""
+    document.getElementById("p2").style.background = "";
+}
