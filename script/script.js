c67ceeabf406fb514ffbb7d456ba769fcc203c59
carlos <carlosking12@gmail.com>
2021-05-19 13:54:48 -0300

    atualizaçao

--- script/script.js
+++ script/script.js
@@ -1,9 +1,7 @@
 function seta_over(obj){
-    obj.innerHTML = "topo";
     document.getElementById("p2").style.background = "red";
 }
 
 function seta_out(obj){
-    obj.innerHTML = ""
     document.getElementById("p2").style.background = "";
 }
